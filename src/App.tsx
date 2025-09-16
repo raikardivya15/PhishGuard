d6fa685f8947cc2ea3efe8759f90d75e43f0f247
Divya Raikar <110682875+raikardivya15@users.noreply.github.com>
2025-09-16 22:27:54 +0530

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <HashRouter> {/* 👈 Changed here */}
+        <HashRouter basename="/ChucklesGalore/"> {/* 👈 Changed here */}
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
